46540ed908004563831365ac98eb0b29d49f423a
Pablo Bernal <pasb@MacBook-Air-de-Pablo-Bernal.local>
2025-02-10 21:06:50 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/San-Valentine/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',  
+  }
 })
